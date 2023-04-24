e417632049a3511a89dd8de51cd4367587da1912
Reshuv <reshu0899@gmail.com>
2023-04-24 14:14:18 +0530

    Todo App

---
+++ Miniproject101/Todominiproject/account/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+
+
+def create_user_account(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email_id")
+        passwd = request.POST.get("passwd")
+
+        if not User.objects.filter(username=username):
+            user = User.objects.create_user(username=username, email=email)
+            user.save()
+            user.set_password(passwd)
+            user.save()
+            return HttpResponse("User created")
+        else:
+            return HttpResponse(f"User with same username ({username}) already exits!")
+
+    return render(request, "Sign_up.html", context={})
+
+def sign_in_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        passwd = request.POST.get("passwd")
+
+        user = authenticate(username=username, password=passwd)
+        if user is not None:
+            login(request, user)
+            return redirect("todo_home")
+            return HttpResponse("User exist in our db")
+        else:
+            return HttpResponse("user does not exist")
+
+    return render(request, "sign_in.html")
+
+def logout_user(request):
+    logout(request)
+
+    return redirect("login")
